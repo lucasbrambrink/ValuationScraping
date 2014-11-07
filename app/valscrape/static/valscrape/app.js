58882128a813bf36ae763f229f980c786fb5c779
lucasbrambrink <lbrambrink@gmail.com>
2014-11-07 15:42:37 -0500

    links to data pages

--- app/valscrape/static/valscrape/app.js
+++ app/valscrape/static/valscrape/app.js
@@ -2,13 +2,14 @@ function list_companies(array) {
 		$('#company_list').empty();
 		for(var i = 0; i < array.length; i++){
 			var element = array[i]
-			$('#company_list').append("<li>"+element+"</li>");
+			$('#company_list').append("<li><a href='/get/"+element+"'>"+element+"</a></li>");
 		}
 	}
 
 
 $(document).ready(function(){
 	$.post()
+
 	$.get('/all', function(data){
 		if(data['error']){
 			alert(data['error'])
