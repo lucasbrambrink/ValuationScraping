19eaddbbd5146e53d6ad5a360b5758702121ef77
lucasbrambrink <lbrambrink@gmail.com>
2014-11-09 14:38:15 -0500

    wont try to ajax the graph, just doesn't work

--- app/valscrape/static/valscrape/app.js
+++ app/valscrape/static/valscrape/app.js
@@ -1,4 +1,4 @@
-var stockGraph = function(value1,value2,name1,name2) {
+function stockGraph(value1,value2,name1,name2) {
 	
 	var barChartData = {
 		labels : [ name1, name2 ],
@@ -8,7 +8,7 @@ var stockGraph = function(value1,value2,name1,name2) {
 				strokeColor : "rgba(220,220,220,0.8)",
 				highlightFill: "rgba(220,220,220,0.75)",
 				highlightStroke: "rgba(220,220,220,1)",
-				data : [value1]
+				data : [value1,value2]
 			},
 			{
 				fillColor : "rgba(151,187,205,0.5)",
@@ -18,7 +18,6 @@ var stockGraph = function(value1,value2,name1,name2) {
 				data : [value2]
 			}
 		]
-
 	}
 	window.onload = function(){
 		var ctx = document.getElementById("canvas").getContext("2d");
@@ -26,8 +25,9 @@ var stockGraph = function(value1,value2,name1,name2) {
 			responsive : true
 		});
 	}
+	console.log('inside')
 }
-
+// stockGraph(10,15,"no","yes")
 
 function list_companies(array) {
 		$('#company_list').empty();
@@ -37,25 +37,35 @@ function list_companies(array) {
 		}
 	}
 
+function trythis() {
+	stockGraph(10,15,"no","yes");
+}
 
 $(document).ready(function(){
+	trythis()
 
-	$.get('/all', function(data){
-		if(data['error']){
-			alert(data['error'])
-		} else { 
-		list_companies(data['stocks']);
-		}
-	}, 'json');
-
+	// $.get('/all', function(data){
+	// 	if(data['error']){
+	// 		alert(data['error'])
+	// 	} else { 
+	// 	list_companies(data['stocks']);
+	// 	}
+	// }, 'json');
+	// stockGraph(10,15,"no","yes")
 	$.get('/get', function(data){
-		console.log('hello')
-		var tuple1 = data['pe'][0]
-		var tuple2 = data['pe'][1]
-		stockGraph(tuple1[0],tuple2[0],tuple1[1],tuple2[1])
-		var element = "<div style='width: 50%''><canvas id='canvas' height='450' width='600'></canvas></div>"
-		$('#graph').append(element);
-		
-	}, 'json');
+		console.log('try')
+		trythis()
+		console.log('past here')
+	},'json');
+		// function(data){
+		// // var element = "<div style='width: 50%''><canvas id='canvas' height='450' width='600'></canvas></div>"
+		// // $('#graph').append(element);
+		// console.log('hello');
+		// var tuple1 = data['pe'][0];
+		// var tuple2 = data['pe'][1];
+		// stockGraph(10,15,"no","yes");
+		// console.log(parseInt(tuple1[1]),parseInt(tuple2[1]),tuple1[0],tuple2[0]);
+		// stockGraph(parseInt(tuple1[1]),parseInt(tuple2[1]),tuple1[0],tuple2[0]);
+
 
 })
